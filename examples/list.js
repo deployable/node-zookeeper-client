1adfcb3cc1e078db3509a58e00f345e94303f570
Matt Hoyle <code@deployable.co>
2017-02-13 12:37:45 +0000

    examples list

--- examples/list.js
+++ examples/list.js
@@ -1,40 +1,23 @@
-/**
- * Copyright (c) 2013 Yahoo! Inc. All rights reserved.
- *
- * Copyrights licensed under the MIT License. See the accompanying LICENSE file
- * for terms.
- */
+const zk = require('..')
+const client = zk.createClient(process.argv[2], { retries : 2 })
+const zkpath = process.argv[3]
 
-var zookeeper = require('../index.js');
-
-var client = zookeeper.createClient(process.argv[2], { retries : 2 });
-var path = process.argv[3];
-
-function listChildren(client, path) {
-    client.getChildren(
-        path,
-        function (event) {
-            console.log('Got watcher event: %s', event);
-            listChildren(client, path);
-        },
-        function (error, children, stat) {
-            if (error) {
-                console.log(
-                    'Failed to list children of node: %s due to: %s.',
-                    path,
-                    error
-                );
-                return;
-            }
-
-            console.log('Children of node: %s are: %j.', path, children);
-        }
-    );
+function listChildren(node_path) {
+  let watcher = (event) => {
+    console.log('Got watcher event: %s', event)
+    listChildren(node_path)
+  }
+  return client.getChildren(node_path, { watcher: watcher }).then(res => {
+    console.log('Children of node: %s are: %j.', node_path, res.children)
+  })
+  .catch(error => {
+    console.log('Failed to list children of node: %s due to: %s.', node_path, error.stack)
+  })
+  .finally(() => client.close())
 }
 
-client.once('connected', function () {
-    console.log('Connected to ZooKeeper.');
-    listChildren(client, path);
-});
+client.connect().then(() => {
+  console.log('Connected to ZooKeeper.')
+  listChildren(zkpath)
+})
 
-client.connect();
